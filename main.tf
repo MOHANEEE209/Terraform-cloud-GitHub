29da28589aff3bfbe6077a3e704fb38f52a2e067
MOHANEEE209 <86760639+MOHANEEE209@users.noreply.github.com>
2021-08-15 15:43:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
+terraform {
+    required_version = "~> 1.0.3" // added latest version
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+     }
+    }
+   }
+
+provider "aws" {
+        # region = "us-east-1" 
+        profile = "default"
+	}
+
+resource "aws_instance" "mohaninfra" {
+	ami = "ami-0c2b8ca1dad447f8a" 
+	instance_type = "t2.micro"
+}
 
